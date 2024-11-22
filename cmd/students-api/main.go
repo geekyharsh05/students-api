80b512c85c26f78794a452e93f85aaf0c03e0e49
geekyharsh05 <hp424805@gmail.com>
2024-11-23 02:22:45 +0530

    implement server setup and configuration loading for students API

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/geekyharsh05/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// load config
+	cfg := config.MustLoad()
+
+	// setup database
+
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started on port http://%s\n", cfg.Address)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed To Start Server")
+	}
+
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 type HTTPServer struct {
-	Address string
+	Address string `yaml:"address" env-required:"true"`
 }
 
 // Using Struct Tags to define the config struct
